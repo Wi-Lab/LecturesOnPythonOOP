8f84ed9e18e04d523c178553e2ea2df435e3bd2c
Sajjad Ranjbar <sajjadranjbar88.5@gmail.com>
2020-05-15 23:18:33 +0430

    Introduction

---
+++ main.py
@@ -0,0 +1,35 @@
+'''
+What Is Object-Oriented Programming (OOP)?
+    Structuring programs so that properties and behaviors are 
+    bundled into individual objects.
+
+    Example:
+        An object could represent a person with a name property,
+        age, address, etc., and behaviors like adding 
+        attachments and sending.
+
+Classes in Python:
+    - Each thing or object is an instance of some class.
+    - The primitive data structures available in Python, like numbers, 
+      strings, and lists.
+    
+    What if you wanted to represent something much more complicated?
+
+    Example:
+        Let’s say you wanted to track a number of different communication node.
+        If we use a list, the first element could be the nodes IP address while 
+            the second element could represent its MAC address.
+        How would we know which element is supposed to be which?
+        What if you had 100 different nodes? 
+    
+    Classes are used to create new user-defined data structures that contain 
+        arbitrary information about something.
+
+    Example:
+        In the case of an communication node, we could create an Node() class 
+            to track properties about the Node like the IP and MAC addresses.
+
+    
+    It’s important to note that a class just provides structure.
+    It’s a blueprint for how something should be defined.    
+'''
