2469d072eac68367ea773673e5c2c406c7c60e8d
Sajjad Ranjbar <sajjadranjbar88.5@gmail.com>
2020-05-16 23:08:23 +0430

    S2

--- main.py
+++ main.py
@@ -1,35 +1,66 @@
 '''
-What Is Object-Oriented Programming (OOP)?
-    Structuring programs so that properties and behaviors are 
-    bundled into individual objects.
+How To Define a Class in Python? 
 
-    Example:
-        An object could represent a person with a name property,
-        age, address, etc., and behaviors like adding 
-        attachments and sending.
+'''
 
-Classes in Python:
-    - Each thing or object is an instance of some class.
-    - The primitive data structures available in Python, like numbers, 
-      strings, and lists.
-    
-    What if you wanted to represent something much more complicated?
 
-    Example:
-        Let’s say you wanted to track a number of different communication node.
-        If we use a list, the first element could be the nodes IP address while 
-            the second element could represent its MAC address.
-        How would we know which element is supposed to be which?
-        What if you had 100 different nodes? 
-    
-    Classes are used to create new user-defined data structures that contain 
-        arbitrary information about something.
+class Node:
+    pass
 
-    Example:
-        In the case of an communication node, we could create an Node() class 
-            to track properties about the Node like the IP and MAC addresses.
 
-    
-    It’s important to note that a class just provides structure.
-    It’s a blueprint for how something should be defined.    
+'''
+We start with the class keyword to indicate that you are creating a class, 
+then you add the name of the class
+
+pass keyword:
+    In Python we use the "pass" keyword (a statement) to indicate that nothing 
+    happens—the function, class or loop is empty.
+
+'''
+
+'''
+Classes create objects, and all objects contain characteristics called attributes.
+
+Use the __init__() method to initialize (e.g., specify) an object’s initial attributes 
+by giving them their default value.
+'''
+
+
+class Packet:
+    # Initializer
+    def __init__(self, dst, payload):
+        self.destination = dst
+        self.payload = payload
+
+
+'''
+ Example:
+    Each packet has a destination attribute and a payload attribute.
+    We use __init__(self,...) method to initialize object attribute.
+
+    self in Packet example is an instance of the class.
+    not all packets have the same destination or payload.
+
+'''
+
+'''
+Class Attributes
+    While instance attributes are specific to each object, 
+    class attributes are the same for all instances- in our case all Packet objects
+'''
+
+
+class Node:
+    # Class Attribute
+    type = 'Node'
+    # Initializer
+
+    def __init__(self, ip, mac):
+        self.ip = ip
+        self.mac = mac
+
+
+'''
+while each Node has a unique ip and mac address, every node object will have a 
+type attribute equal to 'Node'.
 '''
