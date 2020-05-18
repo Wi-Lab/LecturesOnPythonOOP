0774124374dca86a521e25790f5aabb00de5167b
Sajjad Ranjbar <sajjadranjbar88.5@gmail.com>
2020-05-18 12:14:06 +0430

    S6

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 '''
-Instance Methods
+Modifying Attributes
+
+We can change the value of attributes based on some behavior:
 '''
 
 
@@ -7,6 +9,7 @@ class Node:
     def __init__(self, ip, mac):
         self.ip = ip
         self.mac = mac
+        self.queue = []
 
     # instance method
     def description(self):
@@ -16,10 +19,25 @@ class Node:
     def sendPacket(self, payload, dst):
         return "Sending {} to node {}".format(payload, dst.ip)
 
+    # instance method that modifies attributes of instance
+    def receivePacket(self, newPacket):
+        self.queue.append(newPacket)
+
+    def queueLength(self):
+        return "Queue length is {}".format(len(self.queue))
+
+
+'''
+Here, we added a method to receive a packet, which updates the nodes queue.
+'''
 
 n1 = Node('192.168.1.2', 'C9:2A:4E:61:AD:C7')
 n2 = Node('192.168.1.3', 'C9:2A:4E:61:AD:C8')
 
-print(n1.description())
+print(n1.queueLength())
 
-print(n2.sendPacket("MESSAGE", n1))
+n1.receivePacket("Packet 1")
+
+print(n1.queueLength())
+
+n1.receivePacket("Packet 2")
