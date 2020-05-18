60a60f70caafef5f2f4508b0eca1e3113c5ed193
Sajjad Ranjbar <sajjadranjbar88.5@gmail.com>
2020-05-18 11:40:49 +0430

    S5

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-Accessing object attributes
+Instance Methods
 '''
 
 
@@ -8,9 +8,18 @@ class Node:
         self.ip = ip
         self.mac = mac
 
+    # instance method
+    def description(self):
+        return "Node n1 with IP<{}> and MAC<{}>".format(self.ip, self.mac)
+
+    # instance method
+    def sendPacket(self, payload, dst):
+        return "Sending {} to node {}".format(payload, dst.ip)
+
 
 n1 = Node('192.168.1.2', 'C9:2A:4E:61:AD:C7')
 n2 = Node('192.168.1.3', 'C9:2A:4E:61:AD:C8')
 
-print("Node n1 with IP<{}> and MAC<{}>".format(n1.ip, n1.mac))
-print("Node n2 with IP<{}> and MAC<{}>".format(n2.ip, n2.mac))
+print(n1.description())
+
+print(n2.sendPacket("MESSAGE", n1))
