90344bd39c1965500943f0e74c27c9c2f6fa322d
Sajjad Ranjbar <sajjadranjbar88.5@gmail.com>
2020-05-21 08:40:42 +0430

    S9

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
 '''
-Inheritance
-    - Inheritance is the process by which one class takes on the attributes 
-      and methods of another.
-    - Newly formed classes are called child classes.
-
-    - The classes that child classes are derived from are called parent classes.
-
-    - Child classes inherit all of the parent’s attributes and behaviors 
-      but can also specify different behavior to follow.
-
-    - The most basic type of class is an object, which generally all other classes 
-      inherit as their parent.
-
-'''
-
-'''
-class Node(object):
-    pass
-
-# In Python 3, this is the same as:
-
-class Node:
-    pass
+Static Members
 '''
 
 
 class Node:
+    # static member
+    numberOfTotalNodes = 0
+
     def __init__(self, name, ip, mac):
         self.name = name  # Public
         self._ip = ip    # protected
         self._mac = mac  # private
         self.__queue = []  # private
+        Node.numberOfTotalNodes += 1  # increament total node cunter when a new node created
 
     # instance method
     def description(self):
@@ -47,23 +29,14 @@ class Node:
     def queueLength(self):
         return "Queue length is {}".format(len(self.__queue))
 
-
-# WirelessNode inherites all attributes and methods of Node class
-class WirelessNode(Node):
-    def __init__(self, name, ip, mac, radio_range):
-        super().__init__(name, ip, mac)
-        self.radio_range = radio_range  # Public
-
-    # overriding instance method of parent class
-    def description(self):
-        return "WN <{}>, IP<{}>, MAC<{}>,RR<{}>".format(self.name, self._ip, self._mac, self.radio_range)
-
-    def getRadioRange(self):
-        return "RR of <{}> is <{}> meters.".format(self.name, self.radio_range)
+    @staticmethod
+    def totalNodes():
+        return "Total nodes count is: {} ".format(Node.numberOfTotalNodes)
 
 
-n1 = WirelessNode('node 1', '192.168.1.2', 'C9:2A:4E:61:AD:C7', 10)
-n2 = Node('node 2', '192.168.1.3', 'C9:2A:4E:61:AD:C8')
-
-print(n1.description())
-print(n2.description())
+print(Node.totalNodes())
+node1 = Node('node1', "192.168.1.1", 'C9:2A:4E:61:AD:C8')
+print(Node.totalNodes())
+node2 = Node('node2', "192.168.1.2", 'C9:2A:4E:61:BD:C1')
+node3 = Node('node2', "192.168.1.2", 'C9:2A:4E:61:AD:F3')
+print(Node.totalNodes())
