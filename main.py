2aca826a5aefc750f01a309520ba3293ed6d5cce
Sajjad Ranjbar <sajjadranjbar88.5@gmail.com>
2020-05-21 09:11:40 +0430

    S10

--- main.py
+++ main.py
@@ -1,42 +1,35 @@
 '''
-Static Members
+Singleton
 '''
 
 
-class Node:
-    # static member
-    numberOfTotalNodes = 0
-
-    def __init__(self, name, ip, mac):
-        self.name = name  # Public
-        self._ip = ip    # protected
-        self._mac = mac  # private
-        self.__queue = []  # private
-        Node.numberOfTotalNodes += 1  # increament total node cunter when a new node created
-
-    # instance method
-    def description(self):
-        return "Node <{}> with IP<{}> and MAC<{}>".format(self.name, self._ip, self._mac)
-
-    # instance method
-    def sendPacket(self, payload, dst):
-        return "Sending {} to node {}".format(payload, dst._ip)
-
-    # instance method that modifies attributes of instance
-    def receivePacket(self, newPacket):
-        self.queue.append(newPacket)
-
-    def queueLength(self):
-        return "Queue length is {}".format(len(self.__queue))
-
-    @staticmethod
-    def totalNodes():
-        return "Total nodes count is: {} ".format(Node.numberOfTotalNodes)
+'''
+Method __new__ will take class reference as the first argument followed by arguments which are 
+passed to constructor(Arguments passed to call of class to create instance). 
+Method __new__ is responsible to create instance, so you can use this method to customize object creation.
+Typically method __new__ will return the created instance object reference. 
+Method __init__ will be called once __new__ method completed execution.
+'''
 
 
-print(Node.totalNodes())
-node1 = Node('node1', "192.168.1.1", 'C9:2A:4E:61:AD:C8')
-print(Node.totalNodes())
-node2 = Node('node2', "192.168.1.2", 'C9:2A:4E:61:BD:C1')
-node3 = Node('node2', "192.168.1.2", 'C9:2A:4E:61:AD:F3')
-print(Node.totalNodes())
+class OnlyOne:
+
+    # class attribute
+    _instance = None
+
+    def __new__(cls, name):
+        if cls._instance is None:  # this is the first time we create an object of this type
+            cls._instance = super(OnlyOne, cls).__new__(cls)
+
+        return cls._instance
+
+    def __init__(self, name):
+        self.name = name
+
+
+o1 = OnlyOne("node1")
+print(o1)
+print(o1.name)
+o2 = OnlyOne("node2")
+print(o2)
+print(o2.name)
