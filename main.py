dd11aeec223239795ab7a4e41e74ca8434c555cc
Sajjad Ranjbar <sajjadranjbar88.5@gmail.com>
2020-05-26 20:31:05 +0430

    S12_how_to_implement_builder_pattern

--- main.py
+++ main.py
@@ -10,19 +10,21 @@ Builder Pattern is a unique design pattern which helps in building complex objec
 
 
 class Packet:
-    def __init__(self, id, dst):
+    def __init__(self):
+        pass
+
+    def id(self, id):
         self.id = id
+        return self
+
+    def dst(self, dst):
         self.dst = dst
+        return self
 
 
-p1 = Packet(1, 'node1')
-p2 = Packet(1, 'node2')
-p3 = Packet(1, 'node3')
-p4 = Packet(1, 'node4')
-p5 = Packet(1, 'node5')
-p6 = Packet(1, 'node6')
-p7 = Packet(1, 'node7')
-
-'''
-what if we add a new property to our Packet class?
-'''
+p1 = Packet().id(1).dst("node1")
+p2 = Packet().id(2).dst("node2")
+p3 = Packet().id(3).dst("node3")
+p4 = Packet().id(4).dst("node4")
+p5 = Packet().id(5).dst("node5")
+p6 = Packet().id(6).dst("node6")
