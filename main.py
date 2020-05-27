04fc354a2db8743dcf295f1b0c96e4159e5e1851
Sajjad Ranjbar <sajjadranjbar88.5@gmail.com>
2020-05-27 10:26:10 +0430

    S13 why we need factory pattern

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 '''
-Builder Pattern
-'''
-
-
-'''
-Builder Pattern is a unique design pattern which helps in building complex object
- using simple objects and uses an algorithmic approach. 
+Factory Pattern
 '''
 
 
@@ -21,10 +15,26 @@ class Packet:
         self.dst = dst
         return self
 
+    def crc(self, crc):
+        self.crc = crc
+        return self
 
-p1 = Packet().id(1).dst("node1")
+    def fragmentId(self, fragmentId):
+        self.fragmentId = fragmentId
+        return self
+
+    def fragmentCount(self, fragmentCount):
+        self.fragmentCount = fragmentCount
+        return self
+
+    def timestamp(self, timestamp):
+        self.timestamp = timestamp
+        return self
+
+
+p1 = Packet().id(1).dst("node1").crc("FA").fragmentId(1).fragmentCount(3).timestamp(0)
 p2 = Packet().id(2).dst("node2")
-p3 = Packet().id(3).dst("node3")
+p3 = Packet().id(3).dst("node3").crc("FA").fragmentId(1).fragmentCount(4).timestamp(0)
 p4 = Packet().id(4).dst("node4")
 p5 = Packet().id(5).dst("node5")
 p6 = Packet().id(6).dst("node6")
